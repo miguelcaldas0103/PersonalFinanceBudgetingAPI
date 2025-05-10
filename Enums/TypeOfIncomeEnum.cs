34202ce0bc9d368c4840b7a010e472b73efb222b
migue <migue@DESKTOP-9UTK839>
2025-05-10 10:48:42 +0100

    Add project files.

---
+++ Enums/TypeOfIncomeEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceTrackingAPI.Enums
+{
+    public enum TypeOfIncomeEnum
+    {
+        Salary = 1,
+        Investment = 2,
+        Other = 3,
+    }
+}
---
+++ Models/Income.cs
@@ -0,0 +1,15 @@
+﻿using FinanceTrackingAPI.Enums;
+
+namespace FinanceTrackingAPI.Models
+{
+    public class Income
+    {
+        public Guid Id { get; set; }
+        public double Amount { get; set; }
+        public string Title { get; set; }
+        public TypeOfIncomeEnum TypeOfIncome { get; set; }
+        public DateTime DateOfIncome { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
